c8a55ac7c2c162b41abac532c3873ba5c4d47e3a
Liz Daly <liz7daly@gmail.com>
2020-02-19 18:16:48 +0000

    Add jquery callbacks
    A new interface javascript file runs jquery statements to link the html with
    javascript.

--- interface.js
+++ interface.js
@@ -1,4 +1,28 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
-  $('#temperature').text(thermostat.getTemperature());
+  updateTemperature();
+
+  $('#temp-up').on('click', function() {
+    thermostat.increaseTemp();
+    updateTemperature();
+  });
+
+  $('#temp-down').on('click', function() {
+    thermostat.decreaseTemp();
+    updateTemperature();
+  });
+
+  $('#psm-switch').on('click', function() {
+    thermostat.switchMode();
+  });
+
+  $('#reset').on('click', function() {
+    thermostat.reset();
+    updateTemperature();
+  })
+
+  function updateTemperature() {
+    $('#temperature').text(thermostat.getTemperature());
+    $('#temperature').attr('class', thermostat.showEnergyUsage());
+  };
 });
